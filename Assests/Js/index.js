040a91a809f778ff495605df208be3d33479de8f
AbhiDashadiya <pabhi1504@gmail.com>
2024-03-11 23:23:52 +0530

    changes by bhavin

--- Assests/Js/index.js
+++ Assests/Js/index.js
@@ -1,16 +1,11 @@
 const swiper = new Swiper('.swiper', {
-    speed: 1000,
+    speed: 10000,
     autoHeight: true,
     autoplay: {
         delay: 1000,
     },
     centeredSlides : true,
     spaceBetween: 200,
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
 });
