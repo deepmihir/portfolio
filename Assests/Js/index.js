d7fd2d0bf79f6ac793bb2a979fa22e418cf4c9e8
Bhavin Kareliya <bhavin.kareliya2017@gmail.com>
2024-05-08 22:19:00 +0530

    Sidebar full screen issue fixed

--- Assests/Js/index.js
+++ Assests/Js/index.js
@@ -1,10 +1,10 @@
 const swiper = new Swiper('.swiper', {
-	speed: 10000,
-	autoplay: {
-		delay: 1000,
-	},
-	// centeredSlides : true,
-	spaceBetween: 200,
-	slidesPerView: 1,
-	loop: true,
+  speed: 10000,
+  autoplay: {
+    delay: 1000,
+  },
+  // centeredSlides : true,
+  spaceBetween: 200,
+  slidesPerView: 1,
+  loop: true,
 });
